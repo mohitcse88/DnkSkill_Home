79c6698b305b9a627f707c4b4598a716f9788d6b
mohitcse88 <mohitkumardoctor@gmail.com>
2026-04-05 13:03:41 +0530

    Synchronize training page button with home page

--- insert_roadmap.ps1
+++
@@ -1,6 +0,0 @@
-$roadmap = Get-Content roadmap_temp.html -Raw
-$index = Get-Content index.html -Raw
-$target = '<!-- Meet our Mentors Section -->'
-$replacement = $roadmap + "`n`n    " + $target
-$newIndex = $index.Replace($target, $replacement)
-$newIndex | Set-Content index.html -NoNewline
